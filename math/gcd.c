bf2498f2f4079dc6ab5f0e14f475e7d79fafc98c
Phil Dexter <phildexter86@gmail.com>
2025-02-17 08:09:44 -0500

    Added gdc.c

---
+++ math/gcd.c
@@ -0,0 +1,32 @@
+// gcd.c
+// a program to find the greatest common divisor between two integers
+
+#include <stdio.h>
+
+int main()
+{
+    int x;
+    int a, b;
+    int gcd;
+    printf("Enter two integers: ");
+    scanf("%d %d", &a, &b);
+
+    if(a > b)
+        x = a;
+    else
+        x = b;
+    
+    // iterate from 1 to x: find integer that divides them both- greatest divisor will be last. 
+
+    for(int i = 1; i <= x; i++)
+    {
+        if (a % i == 0 && b % i == 0)
+        {
+            gcd = i;
+        }
+    }
+
+    printf("Greatest common divisor is: %d\n", gcd);
+    return 0;
+
+}
