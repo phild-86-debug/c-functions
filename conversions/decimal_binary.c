bd822dfaad36f229deb6155d7519af03d98348be
Phil Dexter <phildexter86@gmail.com>
2025-02-19 06:32:18 -0500

    Added decimal_binary.c

---
+++ conversions/decimal_binary.c
@@ -0,0 +1,32 @@
+// decimal_binary.c
+
+#include <stdio.h>
+
+void decimalToBinary(int n) {
+    if (n == 0) {
+        printf("0");
+        return;
+    }
+
+    int binaryNum[32];
+    int i = 0;
+    while (n > 0) {
+        binaryNum[i] = n % 2;
+        n = n / 2;
+        i++;
+    }
+
+    for (int j = i - 1; j >= 0; j--) {
+        printf("%d", binaryNum[j]);
+    }
+}
+
+int main() {
+    int n;
+    printf("Enter a decimal number: ");
+    scanf("%d", &n);
+    printf("Binary representation: ");
+    decimalToBinary(n);
+    printf("\n");
+    return 0;
+}
