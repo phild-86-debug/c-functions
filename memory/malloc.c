05a37c5f5fe7026188a9141bec2912a4d7306fcc
Phil Dexter <phildexter86@gmail.com>
2025-03-06 09:25:34 -0500

    Added code to free ptr

--- memory/malloc.c
+++ memory/malloc.c
@@ -11,7 +11,7 @@ int *getArray(int n)
     int *ptr = malloc(sizeof(int) * n);
     for(int i = 0; i < n; i++)
     {
-        ptr[i] = i + 1;
+        ptr[i] = i + 1; // *(ptr + i)
     }
     return ptr;
 }
@@ -25,5 +25,7 @@ int main()
         printf("%d ", ptr[i]);
     }
     printf("\n");
+    free(ptr);
+    ptr = NULL;
     return 0;
 }
